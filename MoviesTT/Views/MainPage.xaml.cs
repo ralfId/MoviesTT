0e513455231884d9589a2ef02c2c724d23f058dd
Rafael ID <ralf_raid@outlook.es>
2021-09-10 18:48:08 -0500

    Create mainpage

---
+++ MoviesTT/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MoviesTT.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoviesTT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var vm = new MainPageViewModel();
+            BindingContext = vm;
+        }
+    }
+}
