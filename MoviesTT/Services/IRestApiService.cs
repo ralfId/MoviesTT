66a956155e75dd262cce71307155b4f1667a352f
Rafael ID <ralf_raid@outlook.es>
2021-09-10 18:49:48 -0500

    create an Api service

---
+++ MoviesTT/Services/IRestApiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesTT.Services
+{
+    public interface IRestApiService
+    {
+        Task<T> GetCategory<T>(string category);
+        Task<T> GetMovieDetails<T>(int id);
+
+    }
+}
---
+++ MoviesTT/Services/RestApiService.cs
@@ -0,0 +1,66 @@
+﻿using MoviesTT.Services;
+using MoviesTT.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(RestApiService))]
+namespace MoviesTT.Services
+{
+    public class RestApiService : IRestApiService
+    {
+        HttpClient _httpClient;
+
+        public RestApiService()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(Constants.BaseUrl)
+            };
+        }
+  
+
+        public async Task<T> GetCategory<T>(string category)
+        {
+            try
+            {
+                var rul = $"{category}?api_key={Constants.ApiKey}&language=en-US&page=1";
+                var response = await _httpClient.GetAsync(rul); ;
+                if (response.IsSuccessStatusCode)
+                {
+                    var objs = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(objs);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error to get category: " + ex.ToString());
+            }
+
+            return default(T);
+        }
+
+        public async Task<T> GetMovieDetails<T>(int id)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"{id}?api_key={Constants.ApiKey}&language=en-US"); ;
+                if (response.IsSuccessStatusCode)
+                {
+                    var objs = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(objs);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error to get category: " + ex.ToString());
+            }
+
+            return default(T);
+        }
+    }
+}
