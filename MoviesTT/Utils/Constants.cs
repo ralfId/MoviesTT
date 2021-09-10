8683ef0443cc9d10405d338bbb17496d24704f3a
Rafael ID <ralf_raid@outlook.es>
2021-09-10 18:50:18 -0500

    Add Constants

---
+++ MoviesTT/Utils/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesTT.Utils
+{
+    public class Constants
+    {
+        public const string BaseUrl = "https://api.themoviedb.org/3/movie/";
+        public const string ApiKey = "5dc02a9efdcd5aff197e51b165aef0b8";
+        public const string PopularCategory = "popular";
+
+        public const string ImagesBaseUrl = "https://image.tmdb.org/t/p/w200/";
+
+    }
+}
