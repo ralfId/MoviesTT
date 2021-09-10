b220cb3ba99e055f18773893326a9aaa26cd5d3b
Rafael ID <ralf_raid@outlook.es>
2021-09-10 18:50:44 -0500

    create models

---
+++ MoviesTT/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesTT.Models
+{
+    public class Category
+    {
+        public int page { get; set; }
+        public List<Movie> results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+}
